f4a9779aa9900c7946a8f42d16f4cf067f716afa
JuliaLanfranco <lanfrancojulia@gmail.com>
2025-06-02 09:26:34 -0300

    Agregar funciones de gestión de usuarios
    Se implementaron funciones para registrar usuarios, validar credenciales y modificar roles. Modularizado sin input(). Listo para integrar con el menú principal.

--- usuarios.py
+++ usuarios.py
@@ -1,1 +1,25 @@
-from da
+from datos_de_usuarios import usuarios
+
+def registrar_usuario(nombre, email, contraseña):
+    for u in usuarios:
+        if u ['email'] == email:
+            return False, 'el email ya está registrado'
+    nuevo_usuario = {'nombre' : nombre, 'email' : email, 'contraseña' : contraseña, 'rol' : 'estandar'} #se le asigna rol estandar al registrador.
+    usuarios.append(nuevo_usuario)
+    return True, 'usuario registrado con exito'
+
+def validar_usuario(email,contraseña):
+    for u in usuarios:
+        if u ['email'] == email and u['contraseña'] == contraseña:
+            return True, u 
+    return False, 'email o contraseña incorrectos'
+        
+def modificar_rol(email_usuario, nuevo_rol):
+    roles_validos = ['administrador', 'estandar']
+    if nuevo_rol not in roles_validos:
+        return False, ' rol no valido'
+    for u in usuarios:
+        if u ['email'] == email_usuario:
+            u ['rol'] == nuevo_rol
+            return True, f'rol cambiado a {nuevo_rol} para {u['nombre']}'
+        return False, 'usuario no encontrado'
