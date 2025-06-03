6d677b0de0f99ad89ecc5ed7dd379b8bf0038de8
CarolinaLanfrancocarolinaalanfranco@gmail.com <carolina.a.lanfranco@gmail.com>
2025-06-03 12:55:01 -0300

    Verifica si el usuario ya tiene ese rol antes de intentar cambiarlo

--- usuarios.py
+++ usuarios.py
@@ -17,12 +17,15 @@ def validar_usuario(email,contraseña):
     
 def modificar_rol(email_usuario, nuevo_rol):
     roles_validos = ['administrador', 'estandar']
+    email_usuario = email_usuario.lower().strip()
+    nuevo_rol = nuevo_rol.lower().strip()
     if nuevo_rol not in roles_validos:
-        return False, 'rol no válido'
-    
+        return False, f'rol no válido. Los roles permitidos son: {', '.join(roles_validos)}'
     for u in usuarios:
-        if u['email'] == email_usuario:
-            u['rol'] = nuevo_rol
-            return True, f"rol cambiado a {nuevo_rol} para {u['nombre_completo']}"
+        if u['email'].lower().strip() == email_usuario:
+         if u['rol'] == nuevo_rol:
+            return False, f"El usuario ya tiene asignado el rol '{nuevo_rol}'."
+         u['rol'] = nuevo_rol
+        return True, f"rol cambiado a {nuevo_rol} para {u['nombre_completo']}"
     
     return False, 'usuario no encontrado'
