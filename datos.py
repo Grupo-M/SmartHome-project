0e60753c1ed2177871448d974ee8c2f2cb75b010
DiegoNicolasCordoba <dc6412025@gmail.com>
2025-06-03 19:46:49 -0300

    se agrego una ubicacion

--- datos.py
+++ datos.py
@@ -1,6 +1,6 @@
 dispositivos = [
     {"id": 1, "nombre": "Luz del salón", "estado": "encendido", "esencial": True, "ubicacion": "salón"},
-    {"id": 2, "nombre": "Televisor", "estado": "encendido", "esencial": False,"ubicacion": "salón"},
+    {"id": 2, "nombre": "Televisor", "estado": "encendido", "esencial": False,"ubicacion": "dormitorio"},
     {"id": 3, "nombre": "Router WiFi", "estado": "encendido", "esencial": True,"ubicacion": "salón"},
     {"id": 4, "nombre": "Ventilador", "estado": "encendido", "esencial": False, "ubicacion": "dormitorio"},
 ]
