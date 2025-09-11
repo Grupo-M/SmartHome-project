77228c68c67b5974b03f59c25ffb00d8f1bdb934
Julia Lanfranco <lanfrancojulia@gmail.com>
2025-09-11 10:23:45 -0300

    Tus cambios en usuarios.py trasladados a rama-de-julia

--- usuarios.py
+++ usuarios.py
@@ -4,7 +4,7 @@ def registrar_usuario(nombre_completo, email, contraseña):
     for u in usuarios:
         if u ['email'] == email:
             return False, 'el email ya está registrado'
-    nuevo_usuario = {'nombre_completo' : nombre_completo, 'email' : email, 'contraseña' : contraseña, 'rol' : 'estandar'} #se le asigna rol estandar al registrador.
+    nuevo_usuario = {'nombre_completo' : nombre_completo, 'email' : email, 'contraseña' : contraseña, 'rol' : 2} #se le asigna rol estandar al registrador.
     usuarios.append(nuevo_usuario)
     return True, 'usuario registrado con exito'
 
@@ -20,7 +20,7 @@ def modificar_rol(email_usuario, nuevo_rol):
     email_usuario = email_usuario.lower().strip()
     nuevo_rol = nuevo_rol.lower().strip()
     if nuevo_rol not in roles_validos:
-        return False, f'rol no válido. Los roles permitidos son: {', '.join(roles_validos)}'
+        return False, f'rol no válido. Los roles permitidos son: {roles_validos}'
     for u in usuarios:
         if u['email'].lower().strip() == email_usuario:
          if u['rol'] == nuevo_rol:
